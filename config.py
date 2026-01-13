a1b45439b3be858572a24431bc6826ba19706abf
cyberpravin2 <pravin709121@gmail.com>
2026-01-13 07:17:28 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+# ===============================
+# Music Download Bot - Config
+# ===============================
+
+BOT_TOKEN = "8314402872:AAHES4aT564ifPAwUOpemVS-Fhum2f7-Zd0"
+
+OWNER_ID = 8541949664
+
+COMMAND_PREFIX = "/"
+
+MAX_FILE_SIZE = 50
+
+LOG_CHANNEL = -1003409460306
+# Agar log nahi chahiye:
+# LOG_CHANNEL = -1003409460306
+
+BOT_NAME = "Music Downloader Bot"
+
+# ===============================
+# END OF CONFIG
+# ===============================
